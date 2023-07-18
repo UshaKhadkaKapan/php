133e9943684c970f9e3cbd1c47ea35b57bb59a4e
Usha Khadka Kapan <khadkausha83@gmail.com>
2023-07-18 15:49:06 +1000

    basic php 1

--- index.php
+++ index.php
@@ -9,8 +9,92 @@
 // $student->getEmail();
 
 
-$name = "Usha";
-$age = 23;
-$gender = "Female";
+// $name = "Usha";
+// $age = 23;
+// $gender = "Female";
 
-echo "My name is " . $name . " I am " . $age . ". I am " . $gender;
+// echo "My name is " . $name . " I am " . $age . ". I am " . $gender;
+
+// $x = 5;
+// $name = "Usha";
+// //global variable declaration outside function scope
+
+// var_dump($name);
+
+// define("Name", "Welcome to Programming");
+// echo Name;
+
+// $x = 5;
+// if ($x = 5) {
+//     echo "Statement is true";
+// } else {
+//     echo "statement is false";
+// }
+
+// switch case example
+// $country = "Australia";
+
+// switch ($country) {
+//     case "India":
+//         echo " I love India";
+//         break;
+//     case "Japan":
+//         echo "I like Japan more than any other country.";
+//         break;
+//     case "Australia":
+//         echo " Australians are the best!";
+//         break;
+// }
+
+// $x = 10;
+
+// while ($x <= 15) {
+//     echo 'The value of x :$x<br/>';
+//     ++$x;
+// }
+
+// $x = 10;
+
+// do {
+//     echo ' The Value Of X: ' . $x . ' <br>';
+//     --$x;
+// } while ($x >= 1);
+
+// $x = 1;
+
+// do {
+//     echo 'The number is x :' . $x . '<br/>';
+//     $x++;
+// } while ($x <= 10);
+
+// for ($x = 0; $x <= 10; $x++) {
+//     echo "The value of x: " . $x . '<br/>';
+// }
+
+// for ($x = 0; $x <= 10; $x++) {
+//     if ($x == 5) {
+//         break;
+//     } else {
+//         echo 'The value of x: ' . $x . "<br/>";
+//     }
+// }
+
+// function add($a, $b)
+// {
+//     echo $a + $b;
+// }
+
+// add(1, 2);
+
+// function person($name, $age, $gender)
+// {
+//     echo "My name is " . $name . " and my age is " . $age . " and I am one " . $gender;
+// }
+
+// person("usha", 23, "female");
+
+// To print array, we should use var_dump
+// $flower = array("rose", "lily", "tulip", "lotus");
+// var_dump($flower);
+
+// global scopes
