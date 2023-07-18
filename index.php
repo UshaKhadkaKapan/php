724260fdafde445354804428ae3108ee6fd729e8
Usha Khadka Kapan <khadkausha83@gmail.com>
2023-07-18 16:15:02 +1000

    basic php 2

--- index.php
+++ index.php
@@ -98,3 +98,13 @@
 // var_dump($flower);
 
 // global scopes
+
+// echo $_SERVER['PHP_SELF'];
+// echo "<br>";
+// echo $_SERVER['SERVER_NAME'];
+// echo "<br>";
+// echo $_SERVER['HTTP_HOST'];
+// echo "<br>";
+// echo $_SERVER['HTTP_USER_AGENT'];
+// echo "<br>";
+// echo $_SERVER['SCRIPT_NAME'];
